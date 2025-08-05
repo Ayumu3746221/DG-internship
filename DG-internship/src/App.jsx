c1f21b6dfdb3463de23219651bb3a235ad8f5de9
Akanukko <ogi-yuya0914@docomo.ne.jp>
2025-08-05 12:28:27 +0900

    "Try Graph"

--- DG-internship/src/App.jsx
+++ DG-internship/src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { SalesChart } from "@/components/SalesChart" // 作成したグラフコンポーネントをインポート
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-background text-foreground flex justify-center items-center h-screen p-8">
+      <Card className="w-full max-w-2xl">
+        <CardHeader>
+          <CardTitle>売上ダッシュボード</CardTitle>
+          <CardDescription>月別の合計購入金額の推移です。</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SalesChart />
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
-export default App;
+export default App
