18ac0b9a182e58b3c21234bfe39fa21a784a3612
gosoki <s2122028@stu.musashino-u.ac.jp>
2025-08-05 11:20:51 +0900

    feat: Add ChatPage

--- DG-internship/src/App.jsx
+++ DG-internship/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChatPage from './pages/ChatPage';
 import "./App.css";
 
+/**
+ * メインアプリケーションコンポーネント
+ * ルーティング設定を管理
+ */
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        {/* ルートパス：ホームページ（データ可視化とチャット選択） */}
+          <Route path="/" element={<div>Hello World</div>} />
+          
+          {/* チャットパス：AI分析チャットページ */}
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
